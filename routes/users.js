0df42bfff2f7afc8fdf8eb2844c92bbe2836194b
Herwi <m.herwart@gmail.com>
2017-11-12 12:24:39 +0100

    Trying mysql

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,25 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('users', { title: 'Users' });
+    req.getConnection(function(err,conn){
+        if(err)
+        {
+            console.error('SQL connection error: ', err);
+            return next(err);
+        }
+        else
+        {
+            var selectSQL = "SELECT id, name, surname FROM users";
+            var query = conn.query(selectSQL, function(err,result){
+                if(err)
+                {
+                    console.error('SQL error: ', err);
+                    return next(err);
+                }
+                res.render('users', { title: 'Users', isUsers: true, results: result });
+            });
+        }
+    });
 });
 
 module.exports = router;

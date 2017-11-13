a3c7be9c32f08997922fefdab8dd5ec93a31d48d
Herwi <m.herwart@gmail.com>
2017-11-13 08:33:43 +0100

    Added login form

--- app.js
+++ app.js
@@ -6,8 +6,10 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mysql = require('mysql');
 var connection = require('express-myconnection');
+
 var index = require('./routes/index');
 var users = require('./routes/users');
+var login = require('./routes/login');
 
 var app = express();
 
@@ -34,6 +36,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 //routing
 app.use('/', index);
 app.use('/users', users);
+app.use('/login', login);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
